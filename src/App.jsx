bb06b00f7bc104623df516f6bed2a0f2c256ca8e
bhagyavidevani <152618252+bhagyavidevani@users.noreply.github.com>
2025-05-01 14:24:49 +0530

    router-change

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,37 @@ import Login from './componet/Auth/Login'
 import SingalPage from './componet/SingalPage'
 import Recipe from './componet/Recipe'
 import EditRecipe from './componet/EditRecipe'
-import { useSelector } from 'react-redux'
+import { auth } from './config/firebaseConfig'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const { user } = useSelector((state) => state.AuthReduces);
+  const [user, setUser] = useState(null);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      if (!currentUser && location.pathname !== "/login" && location.pathname !== "/signup") {
+        navigate("/login"); 
+      }
+    });
+    return () => unsubscribe();
+  }, [navigate, location.pathname]);
   return (
     <>
       <Routes>
-        <Route path='/' element={user? <Home/>:<Login/>}/>
-        <Route path='/recipe' element={user?<Recipe/>:<Login/>}/>
-        <Route path='/AddRecipe' element={user?<AddRecipe/>:<Login/>}/>
-        <Route path='/edit/:id' element={user?<EditRecipe/>:<Login/>}/>
-        <Route path='/singalPage/:id' element={user?<SingalPage/>:<Login/>}/>
+        {/* <Route path='/' element={user? <Home/>:<Login/>}/> */}
+        <Route path='/' element={<Home/>}/>
+        {/* <Route path='/recipe' element={user?<Recipe/>:<Login/>}/> */}
+        <Route path='/recipe' element={<Recipe/>}/>
+        {/* <Route path='/AddRecipe' element={user?<AddRecipe/>:<Login/>}/> */}
+        <Route path='/AddRecipe' element={<AddRecipe/>}/>
+        {/* <Route path='/edit/:id' element={user?<EditRecipe/>:<Login/>}/> */}
+        <Route path='/edit/:id' element={<EditRecipe/>}/>
+        {/* <Route path='/singalPage/:id' element={user?<SingalPage/>:<Login/>}/> */}
+        <Route path='/singalPage/:id' element={<SingalPage/>}/>
         <Route path='/login' element={<Login/>}/>
       </Routes>
     </>
